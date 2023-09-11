1c256a3af93bf61d1242e2a3726f8c1a19ecb957
Aqsa Aqeel <85514520+aqsaaqeel@users.noreply.github.com>
2023-09-11 16:25:24 +0530

    Update Chapter 11.js

--- Chapter 11.js
+++ Chapter 11.js
@@ -1,1 +1,72 @@
-C
+/*Q-1: Write an async function locateScalpel that does this, starting at the nest on which it runs. You can use the anyStorage function defined earlier to access storage in arbitrary nests. The scalpel has been going around long enough that you may assume that every nest has a "scalpel" entry in its data storage.
+
+Next, write the same function again without using async and await.
+
+Do request failures properly show up as rejections of the returned promise in both versions? How? */
+
+//Solution:
+
+async function locateScalpel(nest) {
+  let current = nest.name;
+  for (;;) {
+    let next = await anyStorage(nest, current, "scalpel");
+    if (next == current) return current;
+    current = next;
+  }
+}
+
+function locateScalpel2(nest) {
+  function loop(current) {
+    return anyStorage(nest, current, "scalpel").then(next => {
+      if (next == current) return current;
+      else return loop(next);
+    });
+  }
+  return loop(nest.name);
+}
+
+locateScalpel(bigOak).then(console.log);
+// → Butcher's Shop
+locateScalpel2(bigOak).then(console.log);
+// → Butcher's Shop
+
+/*Q-2: Given an array of promises, Promise.all returns a promise that waits for all of the promises in the array to finish. It then succeeds, yielding an array of result values.
+If a promise in the array fails, the promise returned by all fails too, with the failure reason from the failing promise. 
+Implement something like this yourself as a regular function called Promise_all. Remember that after a promise has succeeded or failed, it can’t succeed or fail again, 
+and further calls to the functions that resolve it are ignored. This can simplify the way you handle the failure of your promise.*/
+
+//Solution:
+function Promise_all(promises) {
+  return new Promise((resolve, reject) => {
+    let results = [];
+    let pending = promises.length;
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(result => {
+        results[i] = result;
+        pending--;
+        if (pending == 0) resolve(results);
+      }).catch(reject);
+    }
+    if (promises.length == 0) resolve(results);
+  });
+}
+
+// Test code.
+Promise_all([]).then(array => {
+  console.log("This should be []:", array);
+});
+function soon(val) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(val), Math.random() * 500);
+  });
+}
+Promise_all([soon(1), soon(2), soon(3)]).then(array => {
+  console.log("This should be [1, 2, 3]:", array);
+});
+Promise_all([soon(1), Promise.reject("X"), soon(3)]).then(array => {
+  console.log("We should not get here");
+}).catch(error => {
+  if (error != "X") {
+    console.log("Unexpected failure:", error);
+  }
+});

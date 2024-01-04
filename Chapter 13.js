6bb3382d3a41c852f1be5850467352629dd4749b
Aqsa Aqeel <85514520+aqsaaqeel@users.noreply.github.com>
2024-01-04 23:39:39 +0530

    Update Chapter 13.js

--- Chapter 13.js
+++ Chapter 13.js
@@ -1,1 +1,8 @@
+/*Q-1: Write a function buildTable that, given an array of objects that all have the same set of properties, builds up a DOM structure representing a table. The table should have a header row with the property names wrapped in <th> elements and should have one subsequent row per object in the array, with its property values in <td> elements.
+
+The Object.keys function, which returns an array containing the property names that an object has, will probably be helpful here.
+
+Once you have the basics working, right-align cells containing numbers by setting their style.textAlign property to "right".
+
+*/
 

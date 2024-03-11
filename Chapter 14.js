45a7fc6873541723da74121e76ea0c8e27f7074d
Aqsa Aqeel <85514520+aqsaaqeel@users.noreply.github.com>
2024-03-11 23:20:43 +0530

    Update Chapter 14.js

--- Chapter 14.js
+++ Chapter 14.js
@@ -91,4 +91,53 @@ console.log(byTagName(document.body, "h1").length);
   console.log(byTagName(para, "span").length);
   // → 2
 
-/* Q3: 
+// Q3: Cat with a Hat animation
+//Solution
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Animated Cat with Hat</title>
+  <style>
+    body {
+      min-height: 200px;
+      position: relative;
+    }
+    #cat {
+      position: absolute;
+    }
+    #hat {
+      position: absolute;
+    }
+  </style>
+</head>
+<body>
+  <img src="img/cat.png" id="cat" style="position: absolute">
+  <img src="img/hat.png" id="hat" style="position: absolute">
+
+  <script>
+    let cat = document.querySelector("#cat");
+    let hat = document.querySelector("#hat");
+
+    let angle = 0;
+    let lastTime = null;
+    function animate(time) {
+      if (lastTime != null) angle += (time - lastTime) * 0.001;
+      lastTime = time;
+      cat.style.top = (Math.sin(angle) * 40 + 40) + "px";
+      cat.style.left = (Math.cos(angle) * 200 + 230) + "px";
+
+      hat.style.top = (Math.sin(angle) * + 40) + "px";
+      hat.style.left = (Math.cos(angle) * 210 + 250) + "px";
+
+      // Your extensions here.
+
+      requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+  </script>
+</body>
+</html>
+
